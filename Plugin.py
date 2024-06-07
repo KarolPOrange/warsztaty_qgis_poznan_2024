1620a5a54704959f2ee0ed12655b2ebc1f0b5b66
Karol Puchała <puchala1.karol@gmail.com>
2024-06-07 18:40:16 +0200

    api_z_api_3 - poprawki

--- Plugin.py
+++ Plugin.py
@@ -3,16 +3,18 @@ QGIS Plugin: Api z Api
 """
 import os
 
-from qgis.PyQt.QtGui import QIcon, QAction
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
 from qgis.gui import QgisInterface
+from qgis.utils import iface
 
 from .ApiWindow import ApiWindow
 
 
-class Akcja:
-    ikona = QIcon(os.path.join(os.path.dirname(__file__), 'icons', 'api.svg'))
-    nazwa = "Uruchom wtyczkę Api z Api"
-    opis = "Wtyczka pobierająca za pomocą API i wizualizująca dane o jakości powietrza"
+class ActionRun:
+    name = "Uruchom wtyczkę Api z Api"
+    icon = QIcon(os.path.join(os.path.dirname(__file__), "icons", "api.svg"))
+    parent = iface.mainWindow()
 
 
 class Plugin:
@@ -23,22 +25,30 @@ class Plugin:
 
     def __init__(self, iface: QgisInterface):
         self.iface = iface
-        self.akcja = None
+        self.actionRun = None
 
     def initGui(self):
-        self.akcja = QAction(Akcja.ikona, Akcja.nazwa, self.iface.mainWindow())
-        self.akcja.triggered.connect(self.run)  # uruchamiaj 'run' na kliknięcie ikony z wtyczką
-        self.iface.pluginToolBar().addAction(self.akcja)  # dodaj akcję do paska narzędzi
-        self.iface.pluginMenu().addAction(self.akcja)  # dodaj akcję do menu QGIS
+        """Inicjalizacjia interfejsu wtyczki.
+
+        QGIS najpierw ładuje kolejno wtyczki, uruchamia swoje GUI a na końcu woła kolejno initGui wszystkich wtyczek.
+        """
+        self.actionRun = QAction(
+            ActionRun.icon,
+            ActionRun.name,
+            ActionRun.parent
+        )
+        self.actionRun.triggered.connect(self.run)  # uruchamiaj 'run' na kliknięcie ikony z wtyczką
+        iface.pluginToolBar().addAction(self.actionRun)  # dodaj akcję do paska narzędzi
+        iface.pluginMenu().addAction(self.actionRun)  # dodaj akcję do menu QGIS
 
 
     def unload(self):
-        """Rozładowje wtyczkę. Rozłącza sygnały, zamyka okna należące do wtyczki, usuwa ikonę."""
-        self.akcja.triggered.disconnect(self.run)  # rozłącz sygnał 'run'
-        self.iface.pluginToolBar().removeAction(self.akcja)  # usuń akcję z paska narzędzi
-        self.iface.pluginMenu().removeAction(self.akcja)  # usuń akcję z menu QGIS
+        """Rozładuj wtyczkę."""
+        iface.pluginToolBar().removeAction(self.actionRun)  # usuń akcję z paska narzędzi
+        iface.pluginMenu().removeAction(self.actionRun)  # usuń akcję z menu QGIS
+
 
     def run(self):
-        """Otwiera/zamyka okno"""
-        window = ApiWindow()
-        window.show()
+        """Uruchom okno wtyczki."""
+        window = ApiWindow()  # utworzenie obiektu klasy ApiWindow, tworzone nowe okno, stare nie jest zapamiętane
+        window.show()  # pokaż okno
